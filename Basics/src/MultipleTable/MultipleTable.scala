75a148f74e50ec620f1297967398ec9dbf1e5076
blackicetee <blackicetee@gmx.de>
2016-01-04 12:21:48 +0100

    update multi table

--- Basics/src/MultipleTable/MultipleTable.scala
+++ Basics/src/MultipleTable/MultipleTable.scala
@@ -3,30 +3,21 @@ package MultipleTable
 /**
  * Created by Jason on 13.11.2015.
  */
-class MultipleTable {
-
-  def makeRowSeq(row: Integer) = {
-    for (col <- 1 to 10) yield {
-      val prod = (row * col).toString
-      val padding = " " * (4 - prod.length)
-      padding + prod
-    }
-  }
-
-  def makeRow(row: Integer) = makeRowSeq(row).mkString(", ")
-
-  def createMultiTable() = {
-    val rowSeq =
-      for (row <- 1 to 10) yield {
-        makeRow(row)
-    }
-    rowSeq.toString()
-  }
-}
 
 object MultipleTable {
-  def main (args: Array[String]){
-    val mT = new MultipleTable
-    print(mT.createMultiTable())
+  private def makeRowSeq(row: Int) = {
+    (1 to 10).map(col => (" " * (4 - (col * row).toString.length)) + (col * row).toString)
+  }
+
+  private def makeRow(row: Integer) = makeRowSeq(row).mkString(", ")
+
+  def createMultiTable() = {
+    (1 to 10).map(row => makeRow(row)).mkString("\n");
+  }
+
+  def printGG() = (1 to 10).map(x => println("GG"))
+
+  def main(args: Array[String]) {
+    printGG()
   }
 }
